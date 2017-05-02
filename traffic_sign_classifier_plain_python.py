1df48ff6334e3f5bf3e6ed06bb89762b74633f1d
Juha-Matti Tirilä <juha-matti.tirila@siili.com>
2017-05-02 12:17:14 +0300

    Remove dead code

--- traffic_sign_classifier_plain_python.py
+++ traffic_sign_classifier_plain_python.py
@@ -264,60 +264,9 @@ def _preprocess_data(features, labels, multiply_much=False):
 
 
 def _preprocess_train_test_data(train_features, train_labels, valid_features, valid_labels):
-    """Todo: Initial steps towards some grayscaling etc. Remember, at this point the images have already been shuffled."""
 
-    # TODO: find out ways to preprocess the data in meaningful ways.
-    normalized_train = []
-    normalized_valid = []
-    train_labels = train_labels
-    valid_labels = valid_labels
-
-    # halfway_train = len(X_train) // 2
-    # train_1st_half_copy = X_train[:halfway_train]
-    # train_2st_half_copy = X_train[halfway_train:]
-
-    # labels_train_1st_half = y_train[:halfway_train]
-    # labels_train_2st_half = y_train[halfway_train:]
-
-    # Find the number of occurrences of each of the labels in the training data:
-
-    num_labels = {ind: list(train_labels).count(ind) for ind in set(train_labels)}
-    max_num_labels = max(num_labels.values())
-
-    for label in num_labels.keys():
-        # print("Label: {}".format(label))
-        # Find all images with this label
-        imgs = [img_label[0] for img_label in zip(train_features, train_labels) if img_label[1] == label]
-        coeff = max_num_labels / num_labels[label] - 1
-        coeff_int = int(np.floor(coeff))
-        original_length = len(imgs)
-        if coeff_int > 1:
-            imgs_repeated = [img for img in imgs for _ in range(coeff_int)]
-        else:
-            imgs_repeated = imgs[:]
-        max_ind = int((coeff - coeff_int) * original_length)
-        if max_ind > 0:
-            imgs = np.concatenate((imgs_repeated, imgs[:max_ind]))
-        else:
-            imgs = imgs_repeated
-
-        rotation_angles = [24 * (random() - 0.5) for _ in range(len(imgs))]
-        scale_coeffs = [1 + 0.5 * (random() - 0.5) for _ in range(len(imgs))]
-
-        for ind, img in enumerate(imgs):
-            matr = cv2.getRotationMatrix2D((16, 16), rotation_angles[ind], scale_coeffs[ind])
-            imgs[ind] = cv2.warpAffine(img, matr, (32, 32))
-
-        train_features = np.concatenate([train_features, imgs])
-        train_labels = np.concatenate([train_labels, np.tile(label, len(imgs))])
-
-    # Add a slightly scaled version of the other half of the images, with a little bit of added noise.
-
-    for img in train_features:
-        normalized_train.append(_convert_color_image(img))
-    for img in valid_features:
-        normalized_valid.append(_convert_color_image(img))
-
+    normalized_train, train_labels = _preprocess_data(train_features, train_labels)
+    normalized_valid, valid_labels = _preprocess_data(valid_features, valid_labels)
     return normalized_train, train_labels, normalized_valid, valid_labels
 
 
